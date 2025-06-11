991045ace1aef30a87d34520fe5fd4c6c5810273
Sammy Carolina Arias Riveros <samcaro2233@gmail.com>
2025-06-11 12:39:39 -0500

    Bg Change

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,12 +1,13 @@
 import NavBar from "../Navbar";
 import "./login.css";
 import letter from "./../../assets/letter.png"
+import logo from "./../../assets/logo.png"
 import LoginForm from "../../components/Login-form/LoginForm";
 
 export default function Login() {
     return (
         <>
-            <img src={letter} className="bg" alt="background-image" />
+            <img src={logo} className="bg" alt="background-image" />
             <NavBar />
             <LoginForm />
         </>
